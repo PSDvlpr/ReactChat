83123ada755e7aea4708e8c84ca7a04d3677ce98
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-03-31 19:26:20 +0200

    Import components

--- client/App.js
+++ client/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import io from 'socket.io-client';
 
+import styles from './App.css';
+
+import MessageForm from './MessageForm';
+import MessageList from './MessageList';
+import UsersList from './UsersList';
+import UserForm from './UserForm';
+
 const socket = io('/');
 
 class App extends Component {
@@ -13,7 +20,40 @@ class App extends Component {
         return this.state.name !== '' ? this.renderLayout() : this.renderUserForm();
     }
 
-    renderLayout()
+    renderLayout() {
+        return (
+            <div className={styles.App}>
+                <div className={styles.AppHeader}>
+                    <div className={styles.AppTitle}>
+                        ChatApp
+                    </div>
+                    <div className={styles.AppRoom}>
+                        AppRoom
+                    </div>
+                </div>
+                <div className={styles.AppBody}>
+                    <UsersList
+                        users={this.state.users}
+                    />
+                    <div className={styles.MessageWrapper}>
+                        <MessageList
+                            messages={this.state.messages}
+                        />
+                        <MessageForm
+                            onMessageSubmit={message => this.handleMessageSubmit(message)}
+                            name={this.state.name}
+                        />
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
+    renderUserForm() {
+        return (
+            <UserForm onUserSubmit={name => this.handleUserSubmit(name)} />)
+    }
 };
 
 export default App;
