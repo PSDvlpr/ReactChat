7ad5461ca07f796c7f2f63a3c617262a4d324b43
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-04-01 18:56:29 +0200

    Add & create structure of MessageList

---
+++ client/MessageList.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import styles from './MessageList.css';
+
+const MessageList = props => {
+    <div className={styles.MessageList}>
+        {
+            props.messages.map((message, i) => {
+                return (
+                    <Message
+                        key={i}
+                        from={message.from}
+                        text={message.text}
+                    />
+                );
+            })
+        }
+    </div>
+};
