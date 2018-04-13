958da0cab10e7ecd8712cf3998ce467de9bad9dd
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-04-13 10:37:20 +0200

    Remove

--- client/App.js
+++ client/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import { hot } from 'react-hot-loader';
 import io from 'socket.io-client';
 
 import styles from './App.css';
@@ -89,4 +88,4 @@ class App extends Component {
     }
 };
 
-export default hot(module)(App);
+export default App;
